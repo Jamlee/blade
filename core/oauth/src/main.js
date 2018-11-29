853234eb22a1781119fa3f96673985940305d22a
jamlee <1575103047.com>
2018-11-30 00:54:37 +0800

    feat(module:core): add express log support

--- core/oauth/src/main.js
+++ core/oauth/src/main.js
@@ -5,7 +5,9 @@ const app = express()
 const bodyParser = require('body-parser')
 const router = require('./router')
 const port = process.env.PORT
+const morgan = require('morgan')
 
+app.use(morgan('combined'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.listen(port, () => console.log(`app listening on port ${port}!`))
